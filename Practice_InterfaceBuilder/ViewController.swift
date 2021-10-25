3e887317123f701dc551cd0dfe4139b6c7493c94
Panha <yoeunsopanha2001@gmail.com>
2021-10-25 12:56:19 +0700

    Initial Commit

---
+++ Practice_InterfaceBuilder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice_InterfaceBuilder
+//
+//  Created by sossokleng on 25/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
